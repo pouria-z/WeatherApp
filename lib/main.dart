a05e4af5d26d48e8a2dfdedb5096b1e534bc8d31
Pouria Zeinalzadeh <pouria.zeinalzadeh@gmail.com>
2021-04-18 22:55:47 +0430

    initial

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:weather_app/GetLocation.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: GetLocationWidget(),
+  ));
+}
+
